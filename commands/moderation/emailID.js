3beb209321e9d3a66af02f2d7d22dee805b83133
Mriyam Tamuli <mbtamuli@gmail.com>
2021-07-07 12:18:56 +0530

    :sparkles: NEW: Add commands
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ commands/moderation/emailID.js
@@ -0,0 +1,36 @@
+const {asyncDbRun, ensureUserInDB} = require('../../lib/database');
+
+const addEmailID = async(message, db, logger, args) => {
+	return new Promise(function(resolve, reject) {
+		ensureUserInDB(message, db, logger, args)
+			.then(() => {
+				logger.info(`Update user entry: ${message.author.id} with Email ID: ${args[0]}`);
+				asyncDbRun(db, 'UPDATE users SET email_id=$email_id WHERE id=$id;',{$email_id: args[0],$id: message.author.id})
+					.then(() => {
+						return resolve('updated email ID for user');
+					})
+					.catch(() => {
+						return reject('unable to update email ID for user');
+					});
+			})
+			.catch(() => {
+				return reject('unable to update email ID for user');
+			});
+		
+	});
+};
+
+module.exports = {
+	name: 'emailid',
+	description: 'Add your Email ID to the database.',
+	usage: '<emailid>',
+	cooldown: 5,
+	args: true,
+	guildOnly: true,
+	execute(message, db, logger, args) {
+		addEmailID(message, db, logger, args)
+			.then(() => {
+				message.reply('your Email id was successfully set.');
+			});
+	},
+};
---
+++ commands/moderation/githubID.js
@@ -0,0 +1,36 @@
+const {asyncDbRun, ensureUserInDB} = require('../../lib/database');
+
+const addGitHubID = async(message, db, logger, args) => {
+	return new Promise(function(resolve, reject) {
+		ensureUserInDB(message, db, logger, args)
+			.then(() => {
+				logger.info(`Update user entry: ${message.author.id} with GitHub ID: ${args[0]}`);
+				asyncDbRun(db, 'UPDATE users SET github_id=$github_id WHERE id=$id;',{$github_id: args[0],$id: message.author.id})
+					.then(() => {
+						return resolve('updated github ID for user');
+					})
+					.catch(() => {
+						return reject('unable to update github ID for user');
+					});
+			})
+			.catch(() => {
+				return reject('unable to update github ID for user');
+			});
+		
+	});
+};
+
+module.exports = {
+	name: 'githubid',
+	description: 'Add your github ID to the database.',
+	usage: '<githubID>',
+	cooldown: 5,
+	args: true,
+	guildOnly: true,
+	execute(message, db, logger, args) {
+		addGitHubID(message, db, logger, args)
+			.then(() => {
+				message.reply('your GitHub id was successfully set.');
+			});
+	},
+};
